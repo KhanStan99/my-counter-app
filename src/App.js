5bc614f959a793727e962440a32104156d8ee042
Hidayat Khan <soubankhan3@gmail.com>
2023-08-17 22:23:58 +0530

    added days option

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React, useState } from 'react';
 import './App.css';
 import dayjs from 'dayjs';
 import moment from 'moment';
@@ -8,11 +8,9 @@ import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import Button from '@mui/material/Button';
 
-
 function App() {
-
-  const localData = localStorage.getItem("my_data");
-  const [duration, setDuration] = useState("hours")
+  const localData = localStorage.getItem('my_data');
+  const [duration, setDuration] = useState('hours');
   const [value, setValue] = useState(dayjs(moment()));
 
   let data = [];
@@ -32,93 +30,162 @@ function App() {
 
     formattedData.push({
       dateTime: item,
-      lastTime
+      lastTime,
     });
     average = average + lastTime;
   });
 
-
   average = (average / (formattedData.length - 1)).toFixed(1);
 
   return (
-    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', margin: '12px', }}>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        margin: '12px',
+      }}
+    >
       <div>
         <label>
-          <input type="radio" value="hours" checked={duration === 'hours'} onChange={() => setDuration("hours")} />
+          <input
+            type="radio"
+            value="days"
+            checked={duration === 'days'}
+            onChange={() => setDuration('days')}
+          />
+          Days
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="hours"
+            checked={duration === 'hours'}
+            onChange={() => setDuration('hours')}
+          />
           Hours
         </label>
         <label>
-          <input type="radio" value="minutes" checked={duration === 'minutes'} onChange={() => setDuration("minutes")} />
+          <input
+            type="radio"
+            value="minutes"
+            checked={duration === 'minutes'}
+            onChange={() => setDuration('minutes')}
+          />
           Minutes
         </label>
         <label>
-          <input type="radio" value="seconds" checked={duration === 'seconds'} onChange={() => setDuration("seconds")} />
+          <input
+            type="radio"
+            value="seconds"
+            checked={duration === 'seconds'}
+            onChange={() => setDuration('seconds')}
+          />
           Seconds
         </label>
       </div>
-      <div style={{
-        width: '100%',
-        textAlign: 'center',
-        backgroundColor: "#919191",
-        borderRadius: '20px',
-        margin: '12px',
-        padding: '22px',
-        boxShadow: 'rgb(128, 128, 128) 0px 0px 10px 3px',
-        display: 'flex', flexDirection: 'row', justifyContent: "space-between",
-      }}>
+      <div
+        style={{
+          width: '100%',
+          textAlign: 'center',
+          backgroundColor: '#919191',
+          borderRadius: '20px',
+          margin: '12px',
+          padding: '22px',
+          boxShadow: 'rgb(128, 128, 128) 0px 0px 10px 3px',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+      >
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateTimePicker label="Select date and time"
+          <DateTimePicker
+            label="Select date and time"
             defaultValue={dayjs(moment())}
             value={value}
             onChange={(newValue) => setValue(newValue)}
           />
         </LocalizationProvider>
-        <button style={{ borderRadius: '12px', padding: '8px' }} onClick={() => {
-          data.push(value);
-          localStorage.setItem("my_data", JSON.stringify(data));
-          window.location.reload();
-        }}>Add Now 😔</button>
+        <button
+          style={{ borderRadius: '12px', padding: '8px' }}
+          onClick={() => {
+            data.push(value);
+            localStorage.setItem('my_data', JSON.stringify(data));
+            window.location.reload();
+          }}
+        >
+          Add Now 😔
+        </button>
       </div>
 
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        textAlign: 'center'
-      }}>
-        {Number(average) ? <>Average of {average} {duration}</> : null}
-        <p>{data.length > 0 && <>Since last record:  {String(`${moment().diff(moment(data[data.length - 1]), duration)} ${duration}`)}</>}</p>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          textAlign: 'center',
+        }}
+      >
+        {Number(average) ? (
+          <>
+            {formattedData.length} events in last{' '}
+            {moment().diff(moment(formattedData[0].dateTime), duration)} {duration} with
+            an average of {average} {duration}
+          </>
+        ) : null}
+        <p>
+          {data.length > 0 && (
+            <>
+              Since last record:{' '}
+              {String(
+                `${moment().diff(
+                  moment(data[data.length - 1]),
+                  duration
+                )} ${duration}`
+              )}
+            </>
+          )}
+        </p>
       </div>
 
-      {
-        formattedData.length > 0 && formattedData.map((time, index) => {
-          return <div key={index} style={{
-            width: '100%',
-            textAlign: 'center',
-            backgroundColor: "#000",
-            borderRadius: '180px',
-            margin: '12px',
-            padding: '12px',
-            boxShadow: 'rgb(128, 128, 128) 0px 0px 10px 3px'
-          }}>
-            {index > 0 ?
-              <>
-                After {String(`${time.lastTime} ${duration}`)}
-                <br />
-              </> : null}
-
-            {moment(time.dateTime).format("DD-MM-YYYY # hh:mm:ss a")}
-
-            <Button variant="contained"
-              onClick={() => {
-                data.splice(index, 1);
-                localStorage.setItem("my_data", JSON.stringify(data));
-                window.location.reload();
+      {formattedData.length > 0 &&
+        formattedData.map((time, index) => {
+          return (
+            <div
+              key={index}
+              style={{
+                width: '100%',
+                textAlign: 'center',
+                backgroundColor: '#000',
+                borderRadius: '180px',
+                margin: '12px',
+                padding: '12px',
+                boxShadow: 'rgb(128, 128, 128) 0px 0px 10px 3px',
               }}
-              style={{ marginLeft: '12px', padding: '12px' }} ><DeleteForeverIcon /></Button>
-          </div>
-        })
-      }
-    </div >
+            >
+              {index > 0 ? (
+                <>
+                  After {String(`${time.lastTime} ${duration}`)}
+                  <br />
+                </>
+              ) : null}
+
+              {moment(time.dateTime).format('DD-MM-YYYY # hh:mm:ss a')}
+
+              <Button
+                variant="contained"
+                onClick={() => {
+                  data.splice(index, 1);
+                  localStorage.setItem('my_data', JSON.stringify(data));
+                  window.location.reload();
+                }}
+                style={{ marginLeft: '12px', padding: '12px' }}
+              >
+                <DeleteForeverIcon />
+              </Button>
+            </div>
+          );
+        })}
+    </div>
   );
 }
 
